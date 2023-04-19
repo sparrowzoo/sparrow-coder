b998ff28e99c1c0291f9654601ef148e9c585bc0
harry <zh_harry@163.com>
2023-04-19 17:29:22 +0800

    新增框架复制功能

--- coder/src/main/java/com/sparrow/coding/ProjectCopier.java
+++ coder/src/main/java/com/sparrow/coding/ProjectCopier.java
@@ -1,40 +1,87 @@
 package com.sparrow.coding;
 
 import com.sparrow.utility.FileUtility;
-import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ProjectCopier {
     private static Logger logger = LoggerFactory.getLogger(ProjectCopier.class);
 
-    public static void main(String[] args2) {
-        String[] args = new String[6];
-        args[0] = "/Users/zhanglizhi/workspace/tedu/cs-mall-shopping-cart";
-        args[1] = "/Users/zhanglizhi/workspace/tedu/cs-mall-order";
-        args[2] = "/Users/zhanglizhi/workspace/tedu/cs-mall-pay-center";
-        args[3] = "/Users/zhanglizhi/workspace/tedu/cs-mall-user";
-        args[4] = "/Users/zhanglizhi/workspace/tedu/inn-component";
-        args[5] = "/Users/zhanglizhi/workspace/tedu/cs-mall-shopping-cart-wrap";
-        String target = args[args.length - 1];
-        for (int i = 0; i < args.length - 1; i++) {
-            String source = args[i];
+    private static String workspace;
+    private static String target;
+    private static String resourceTarget;
+
+    public static void run(String config) throws IOException {
+        Properties properties = System.getProperties();
+        properties.load(ProjectCopier.class.getResourceAsStream("/tedu/" + config));
+
+        workspace = properties.getProperty("workspace");
+        String sources = properties.getProperty("sources");
+        String[] sourceProjects = sources.split(",");
+        target = workspace + properties.getProperty("target");
+        resourceTarget = workspace + properties.getProperty("resource_target");
+        for (String source : sourceProjects) {
             try {
-                copy(source, target);
+                copy(workspace + source);
             } catch (Exception e) {
                 System.out.println(source + " is error");
             }
         }
     }
 
-    private static void copy(String source, String target) {
+    public static void main(String[] args2) throws IOException {
+        run("copier.properties");
+        //run("admin_copier.properties");
+    }
+
+    private static FileUtility.FolderFilter filter = (sourceFile, targetFile) -> {
+        if (sourceFile.equalsIgnoreCase("target")) {
+            return true;
+        }
+        //main 排除
+        if (sourceFile.startsWith(".")) {
+            return true;
+        }
+        if (sourceFile.endsWith("-main")) {
+            return true;
+        }
+        return false;
+    };
+
+    private static void copy(String source) {
         File directory = new File(source);
         String[] directoryList = directory.list();
+        if (directoryList == null) {
+            return;
+        }
         for (String childDirectory : directoryList) {
+            if (filter.filter(childDirectory, target)) {
+                continue;
+            }
+
+            if (childDirectory.contains("dao-impl")) {
+                String mapperSource = source + File.separator + childDirectory + "/src/main/resources";
+                File sourceDirectory = new File(mapperSource);
+                if (sourceDirectory.exists()) {
+                    FileUtility.getInstance().recurseCopy(mapperSource, resourceTarget);
+                } else {
+                    logger.warn("{} is not exist", mapperSource);
+                }
+                continue;
+            }
+
+
             String sourceDirectoryPath = source + File.separator + childDirectory + "/src/main/java";
             File sourceDirectory = new File(sourceDirectoryPath);
             if (sourceDirectory.exists()) {
+                logger.info("copy {} to {}", sourceDirectoryPath, target);
                 FileUtility.getInstance().recurseCopy(sourceDirectoryPath, target);
+            } else {
+                logger.warn("{} is not exist", sourceDirectoryPath);
             }
         }
     }
