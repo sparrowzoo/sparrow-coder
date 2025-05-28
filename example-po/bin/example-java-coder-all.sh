1b48b1b6737ccfe599beaf50635e67b6f4296bd5
张立志 <zhanglizhi@tedu.cn>
2025-05-28 10:57:24 +0800

    新增框架聚合复制功能

--- example-po/bin/example-java-coder-all.sh
+++ example-po/bin/example-java-coder-all.sh
@@ -1,3 +1,12 @@
 config_path=$(pwd)/config.properties
 echo $config_path
-sh ./sparrow-java-coder.sh -all com.sparrow.example.po.SparrowExample -config=$config_path
+
+if pwd | grep -q -E 'bin$'; then
+    echo "true"
+else
+    echo "请cd到bin目录下运行"
+    exit 1
+fi
+
+echo $pwd
+#sh ./sparrow-java-coder.sh -all com.sparrow.example.po.SparrowExample -config=$config_path
