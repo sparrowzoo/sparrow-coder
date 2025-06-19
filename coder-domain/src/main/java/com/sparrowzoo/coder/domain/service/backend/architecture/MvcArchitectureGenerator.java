3d3c7783ff5793bb56a0299d0977d440b9770323
zh_harry <zh_harry@163.com>
2025-06-20 00:04:35 +0800

    add

---
+++ coder-domain/src/main/java/com/sparrowzoo/coder/domain/service/backend/architecture/MvcArchitectureGenerator.java
@@ -0,0 +1,24 @@
+package com.sparrowzoo.coder.domain.service.backend.architecture;
+
+import com.sparrowzoo.coder.domain.service.AbstractArchitectureGenerator;
+import com.sparrowzoo.coder.domain.service.registry.TableConfigRegistry;
+import com.sparrowzoo.coder.enums.ArchitectureCategory;
+
+import java.io.IOException;
+
+public class MvcArchitectureGenerator extends AbstractArchitectureGenerator {
+    @Override
+    public void generate(TableConfigRegistry registry, String tableName) throws IOException {
+
+    }
+
+    @Override
+    public ArchitectureCategory getCategory() {
+        return null;
+    }
+
+    @Override
+    public String template() {
+        return null;
+    }
+}
