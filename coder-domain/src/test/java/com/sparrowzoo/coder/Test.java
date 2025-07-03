c5c8ee53e1888cf48b98b4ee9809441855583ec2
zh_harry <zh_harry@163.com>
2025-07-03 21:33:23 +0800

    add

---
+++ coder-domain/src/test/java/com/sparrowzoo/coder/Test.java
@@ -0,0 +1,8 @@
+package com.sparrowzoo.coder;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("%d",(3,5));
+        System.out.println("Hello World!");
+    }
+}
